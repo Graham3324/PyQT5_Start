032805e52e9f73398a8d6363cf4ecf1bf6e50472
Graham3324 <Graham.Sayers@yahoo.com>
2020-04-18 09:19:12 -0400

    Edits

---
+++ PyQT5_Template/PyQT5_Template.py
@@ -0,0 +1,25 @@
+import sys
+
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
+
+
+class MainWindow(qtw.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Your code goes here
+
+        # Your code ends here
+
+        self.show()
+
+
+if __name__ == "__main__":
+
+    app = qtw.QApplication(sys.argv)
+
+    w = MainWindow(windowTitle="hello world")
+    w.show()
+
+    sys.exit(app.exec_())
